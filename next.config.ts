8a3a5665c85c033b6f06edf3df4276927a516135
Miappvzl <miappvzl@gmail.com>
2026-03-31 22:17:16 -0400

    eliminando el permiso a vercel para que optimice imagenes

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // 🚀 BYPASS: Apaga el motor de cobro de Vercel
     remotePatterns: [
       {
         protocol: 'https',
-        // 🚀 PROYECTO NUEVO (Para todas las imágenes y pedidos futuros)
         hostname: 'qzeelmmhictsabuwbyjh.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
       {
         protocol: 'https',
-        // 🕰️ PROYECTO VIEJO (Retrocompatibilidad para cargar recibos históricos)
         hostname: 'lrmhgzohfclrepwvrhdy.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
