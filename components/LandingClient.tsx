0ee353e56c67edd5cf398f39c46fa63b26ddd09f
Miappvzl <miappvzl@gmail.com>
2026-06-05 15:34:49 -0400

    correccion de url de imagen de la landing, especificamnte la del hoodie de titanio

--- components/LandingClient.tsx
+++ components/LandingClient.tsx
@@ -919,7 +919,7 @@ const NodeFourNeural = ({ bcvRate }: { bcvRate: number }) => {
 
               {/* TU IMAGEN PNG CENTRADA */}
               <img
-                src="hoodietitanio.png"
+                src="hoodietitanio.webp"
                 alt="Producto"
                 className="h-4/5 w-auto object-contain z-10 transition-transform duration-500 group-hover:scale-105"
               />
