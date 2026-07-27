d0a6f76da96e10544da69453d5062fe46f5a3125
Miappvzl <miappvzl@gmail.com>
2026-07-27 00:13:46 -0400

    volviendo a corregir icono

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,21 +7,11 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'El sistema inteligente de gestión y ventas multimoneda.',
     start_url: '/admin',
     display: 'standalone',
-    background_color: '#0d0d0d', // Mantén el fondo negro para el Splash Screen
-    theme_color: '#0d0d0d',
-    icons: [
-      {
-        src: '/favicon-dark.png', // 👈 Para la Pantalla de Carga (Z Blanca)
-        sizes: '192x192 512x512',
-        type: 'image/png',
-        purpose: 'any',
-      },
-      {
-        src: '/favicon-light.png', // 👈 Para el Icono del Escritorio (Z Negra)
-        sizes: '192x192 512x512',
-        type: 'image/png',
-        purpose: 'maskable',
-      },
-    ],
+    background_color: '#0d0d0d',
+theme_color: '#0d0d0d',
+icons: [
+  { src: '/favicon-dark.png', sizes: '192x192 512x512', type: 'image/png', purpose: 'any' },
+  { src: '/favicon-dark.png', sizes: '192x192 512x512', type: 'image/png', purpose: 'maskable' }
+]
   }
 }
