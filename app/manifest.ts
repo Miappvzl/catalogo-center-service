ae6d9f2d806612391baddb61ad2d3d7b131c9428
Miappvzl <miappvzl@gmail.com>
2026-07-26 23:46:41 -0400

    corrigiendo el logo que se renderiza en la pantalla de apertura de la app

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,17 +7,17 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'El sistema inteligente de gestión y ventas multimoneda.',
     start_url: '/admin',
     display: 'standalone',
-    background_color: '#0d0d0d', // Mantén el fondo negro para el Splash Screen de noche
-    theme_color: '#0d0d0d',
+    background_color: '#0d0d0d', // Fondo negro de alta gama para evitar flashazos de noche
+    theme_color: '#0d0d0d',      // Sincronizado
     icons: [
       {
-        src: '/favicon-light.png', // 👈 Volvemos a tu icono original gigante
+        src: '/favicon-dark.png', // 👈 APUNTAMOS A LA "Z" BLANCA (favicon-dark)
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/favicon-light.png', // 👈 Volvemos a tu icono original gigante
+        src: '/favicon-dark.png', // 👈 APUNTAMOS A LA "Z" BLANCA (favicon-dark)
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
